651a6ce6c7bddef9d04e66f0d3fb1d23e53faa57
moseung <tmdah900@naver.com>
2022-12-20 22:50:15 +0900

    fix: 버셀 빌드 안되는 버그 수정 -forceConsistentCasingInFileNames = false로 수정

---
+++ src/routes/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AddRecord from '@pages/AddRecord/AddRecord'
+import DetailRecord from '@pages/DetailRecord/DetailRecord'
+import Main from '@pages/Main/Main'
+import NotFound from '@pages/NotFound/NotFound'
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/record/add" element={<AddRecord />} />
+        <Route path="/record/:recordId" element={<DetailRecord />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
