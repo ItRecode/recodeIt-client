f570f3b0fd411542875a4e30a17f64bbe317a944
Seongtaek Hwang <hst1993@hanmail.net>
2022-12-22 02:05:52 +0900

    [FE-9] feat: navbar css 완성

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,43 @@
 import React from 'react'
+import { ReactComponent as Home_icon } from '@assets/nav_icons/home_icon.svg'
+import { ReactComponent as Lank_icon } from '@assets/nav_icons/lank_icon.svg'
+import { ReactComponent as MyRecord_icon } from '@assets/nav_icons/myrecord_icon.svg'
+import { ReactComponent as Config_icon } from '@assets/nav_icons/config_icon.svg'
+import { ReactComponent as Record_icon } from '@assets/nav_icons/record_icon.svg'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
-  return <div>index</div>
+  return (
+    <div className="absolute bottom-0 flex h-[60px] w-[375px] justify-between rounded-t-xl border border-solid border-grey-3 px-3 pt-1.5">
+      <div className="left-3 flex">
+        <div className="group mr-2.5 flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
+          <Home_icon className="fill-grey-3 group-hover:fill-primary-2 " />
+          <p className="text-xs text-grey-3  group-hover:text-primary-2">홈</p>
+        </div>
+        <div className="group flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
+          <Lank_icon className="fill-grey-3 group-hover:fill-primary-2" />
+          <p className="text-xs text-grey-3  group-hover:text-primary-2">
+            랭킹
+          </p>
+        </div>
+      </div>
+      <div className="relative bottom-[50px] cursor-pointer">
+        <Record_icon />
+      </div>
+      <div className="right-3 flex">
+        <div className="group mr-2.5 flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
+          <MyRecord_icon className="fill-grey-3 group-hover:fill-primary-2" />
+          <p className="text-xs text-grey-3  group-hover:text-primary-2">
+            마이레코드
+          </p>
+        </div>
+        <div className="group flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
+          <Config_icon className="fill-grey-3 group-hover:fill-primary-2" />
+          <p className="text-xs text-grey-3  group-hover:text-primary-2">
+            설정
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
