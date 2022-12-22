f9b915391e79a82c193e1f6dd6e4293ae4c9b410
waterglasses <hsk10271@gmail.com>
2022-12-22 22:57:29 +0900

    [FE-37] feat: Axios Instance 구현

---
+++ src/apis/instance.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import { getItem } from '@utils/localStorage'
+import { redirect } from 'react-router-dom'
+
+const { REACT_APP_END_POINT } = process.env
+
+const baseInstance = axios.create({ baseURL: REACT_APP_END_POINT })
+const authInstance = axios.create({ baseURL: REACT_APP_END_POINT })
+
+authInstance.interceptors.request.use((config) => {
+  config.headers = {
+    authorization: `Bearer ${getItem('token', '')}`,
+  }
+  return config
+})
+
+authInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      redirect('/login')
+      return
+    }
+
+    console.error(error)
+    return Promise.reject(error)
+  }
+)
+
+export { baseInstance, authInstance }
