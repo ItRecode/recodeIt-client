e5626f86fb5fd52a7fb51addbc61935ba6e753c9
waterglasses <hsk10271@gmail.com>
2022-12-22 14:41:56 +0900

    [FE-30] chore: eslint no-console 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
     indent: 'off',
     '@typescript-eslint/no-var-requires': 0,
     'react/self-closing-comp': 'warn', // 셀프 클로징 태그 가능하면 적용
+    'no-console': 'warn',
   },
 }
