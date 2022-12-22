fd6919ea49bc3a38c8145b02da65ee3fe522a612
waterglasses <hsk10271@gmail.com>
2022-12-23 00:58:53 +0900

    [FE-37] refactor: Axios interceptor 리팩토링

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,29 +1,32 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 import { getItem } from '@utils/localStorage'
 import { redirect } from 'react-router-dom'
 
 const { REACT_APP_DEV_API_END_POINT } = process.env
 
-const baseInstance = axios.create({ baseURL: REACT_APP_DEV_API_END_POINT })
-const authInstance = axios.create({ baseURL: REACT_APP_DEV_API_END_POINT })
-
-authInstance.interceptors.request.use((config) => {
-  config.headers = {
-    authorization: `Bearer ${getItem('token', '')}`,
-  }
-  return config
-})
-
-authInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401) {
-      redirect('/login')
+function setInterceptors(instance: AxiosInstance) {
+  instance.interceptors.request.use((config) => {
+    config.headers = {
+      authorization: `Bearer ${getItem('token', '')}`,
     }
+    return config
+  })
 
-    console.error(error)
-    return Promise.reject(error)
-  }
-)
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (error.response?.status === 401) {
+        redirect('/login')
+      }
+
+      console.error(error)
+      return Promise.reject(error)
+    }
+  )
+  return instance
+}
+
+const baseInstance = axios.create({ baseURL: REACT_APP_DEV_API_END_POINT })
+const authInstance = setInterceptors(baseInstance)
 
 export { baseInstance, authInstance }
