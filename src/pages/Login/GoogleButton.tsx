bccfcbd8acb9b126818ff252f4852127c43e0308
waterglasses <hsk10271@gmail.com>
2022-12-22 14:16:59 +0900

    [FE-10] style: button에 aria-label 추가

--- src/pages/Login/GoogleButton.tsx
+++ src/pages/Login/GoogleButton.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as GoogleSymbol } from '@assets/google.svg'
 export default function GoogleButton() {
   return (
     <button
-      className="bg-grey-1 flex items-center rounded-xl h-12 px-8 font-semibold
-        w-85 border-none hover:cursor-pointer"
+      aria-label="google-login-button"
+      className="flex items-center h-12 px-8 font-semibold border-none w-85 rounded-xl bg-grey-1 hover:cursor-pointer"
     >
       <GoogleSymbol aria-label="google-symbol-icon" />
       <p className="w-full text-kakao-label">Google로 계속하기</p>
--- src/pages/Login/KakaoButton.tsx
+++ src/pages/Login/KakaoButton.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as KakaoSymbol } from '@assets/kakao.svg'
 export default function KakaoButton() {
   return (
     <button
-      className="bg-kakao flex items-center rounded-xl h-12 px-8 font-semibold
-        w-85 border-none hover:cursor-pointer"
+      aria-label="kakao-login-button"
+      className="flex items-center h-12 px-8 font-semibold border-none w-85 rounded-xl bg-kakao hover:cursor-pointer"
     >
       <KakaoSymbol aria-label="kakao-symbol-icon" />
       <p className="w-full text-kakao-label">카카오로 계속하기</p>
