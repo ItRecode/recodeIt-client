338d6f541a05b5c49c486322c730c560170d4a36
waterglasses <hsk10271@gmail.com>
2022-12-22 23:11:05 +0900

    [FE-37] etc: 인스턴스 사용 예시 로그인 예시 추가

---
+++ src/apis/auth.ts
@@ -0,0 +1,8 @@
+import { authInstance } from './instance'
+
+// instance 사용 예시
+// export const login = (token: string, type: string) => {
+//   authInstance.post(`/member/oauth/login/${type}`, {
+//     token,
+//   })
+// }
