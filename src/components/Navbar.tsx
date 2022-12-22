18184886456ef32f54ddc61df4881b0d7474471d
Seongtaek Hwang <hst1993@hanmail.net>
2022-12-22 15:27:14 +0900

    [FE-9] feat: Navbar 컴포넌트 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,42 +2,67 @@ import React from 'react'
 import { ReactComponent as Home_icon } from '@assets/nav_icons/home_icon.svg'
 import { ReactComponent as Lank_icon } from '@assets/nav_icons/lank_icon.svg'
 import { ReactComponent as MyRecord_icon } from '@assets/nav_icons/myrecord_icon.svg'
-import { ReactComponent as Config_icon } from '@assets/nav_icons/config_icon.svg'
+import { ReactComponent as Setting_icon } from '@assets/nav_icons/setting_icon.svg'
 import { ReactComponent as Record_icon } from '@assets/nav_icons/record_icon.svg'
-import { Link } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 export default function NavBar() {
+  const { pathname } = useLocation()
+
+  const checkText = (name: string) => {
+    if (pathname === `${name}`) {
+      return 'text-primary-2'
+    } else return ''
+  }
+
+  const checkIcon = (name: string) => {
+    if (pathname === `${name}`) {
+      return 'fill-primary-2'
+    } else return ''
+  }
+
+  const containerFormat =
+    'group flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer'
+  const iconFormat = 'fill-grey-3 group-hover:fill-primary-2'
+  const textFormat = 'text-xs text-grey-3  group-hover:text-primary-2'
+
   return (
-    <div className="absolute bottom-0 flex h-[60px] w-[375px] justify-between rounded-t-xl border border-solid border-grey-3 px-3 pt-1.5">
-      <div className="left-3 flex">
-        <div className="group mr-2.5 flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
-          <Home_icon className="fill-grey-3 group-hover:fill-primary-2 " />
-          <p className="text-xs text-grey-3  group-hover:text-primary-2">홈</p>
+    <>
+      <Outlet />
+      <div className="absolute bottom-0 flex h-[60px] w-[375px] justify-between rounded-t-xl border border-solid border-grey-3 px-3 pt-1.5">
+        <div className="left-3 flex">
+          <Link to="/" className={`${containerFormat} mr-5`}>
+            <Home_icon className={`${iconFormat} ${checkIcon('/')}`} />
+            <p className={`${textFormat} ${checkText('/')}`}>홈</p>
+          </Link>
+          <Link to="/rank" className={containerFormat}>
+            <Lank_icon className={`${iconFormat} ${checkIcon('/rank')}`} />
+            <p className={`${textFormat} ${checkText('/rank')}`}>랭킹</p>
+          </Link>
         </div>
-        <div className="group flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
-          <Lank_icon className="fill-grey-3 group-hover:fill-primary-2" />
-          <p className="text-xs text-grey-3  group-hover:text-primary-2">
-            랭킹
-          </p>
+        <Link
+          to="/record/add"
+          className="relative bottom-[50px] cursor-pointer"
+        >
+          <Record_icon />
+        </Link>
+        <div className="right-3 flex">
+          <Link to="/myrecord" className={`${containerFormat} mr-5`}>
+            <MyRecord_icon
+              className={`${iconFormat} ${checkIcon('/myrecord')}`}
+            />
+            <p className={`${textFormat} ${checkText('/myrecord')}`}>
+              마이레코드
+            </p>
+          </Link>
+          <Link to="/setting" className={containerFormat}>
+            <Setting_icon
+              className={`${iconFormat} ${checkIcon('/setting')}`}
+            />
+            <p className={`${textFormat} ${checkText('/setting')}`}>설정</p>
+          </Link>
         </div>
       </div>
-      <div className="relative bottom-[50px] cursor-pointer">
-        <Record_icon />
-      </div>
-      <div className="right-3 flex">
-        <div className="group mr-2.5 flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
-          <MyRecord_icon className="fill-grey-3 group-hover:fill-primary-2" />
-          <p className="text-xs text-grey-3  group-hover:text-primary-2">
-            마이레코드
-          </p>
-        </div>
-        <div className="group flex h-full w-[54px] flex-col items-center justify-items-center hover:cursor-pointer">
-          <Config_icon className="fill-grey-3 group-hover:fill-primary-2" />
-          <p className="text-xs text-grey-3  group-hover:text-primary-2">
-            설정
-          </p>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,12 +4,21 @@ import AddRecord from '@pages/AddRecord/AddRecord'
 import DetailRecord from '@pages/DetailRecord/DetailRecord'
 import Main from '@pages/Main/Main'
 import NotFound from '@pages/NotFound/NotFound'
+import Rank from '@pages/Rank/Rank'
+import MyRecord from '@pages/MyRecord/MyRecord'
+import Setting from '@pages/Setting/Setting'
+import NavBar from '@components/Navbar'
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Main />} />
+          <Route path="rank" element={<Rank />} />
+          <Route path="myrecord" element={<MyRecord />} />
+          <Route path="setting" element={<Setting />} />
+        </Route>
         <Route path="/record/add" element={<AddRecord />} />
         <Route path="/record/:recordId" element={<DetailRecord />} />
         <Route path="*" element={<NotFound />} />
