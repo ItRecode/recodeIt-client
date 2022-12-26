9055afb299d7c4d2f87bca7641b076b811b60f04
moseung <tmdah900@naver.com>
2022-12-26 18:16:40 +0900

    [FE-49]feat: 파일 추가 기능구현 -파일 추가 기능구현 -관련 디자인도 구현

--- src/pages/AddRecord/AddRecordFile.tsx
+++ src/pages/AddRecord/AddRecordFile.tsx
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Camera from '@assets/camera.svg'
+import { ReactComponent as Xbutton } from '@assets/Xbutton.svg'
 
 function AddRecordFile() {
+  const [currentImg, setCurrentImg] = useState<null | string>(null)
+
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    encodeFileToBase64((e.target.files as FileList)[0])
+    setCurrentImg(e.target.value)
+  }
+
+  const encodeFileToBase64 = (fileBlob: File) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(fileBlob)
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        setCurrentImg(reader.result as string | null)
+        resolve()
+      }
+    })
+  }
+
   return (
-    <div>
+    <div className="flex items-center">
       <label htmlFor="file">
-        <div className="flex h-[66px] w-[66px] flex-col items-center justify-center  rounded-2xl border-2 border-dashed border-gray-400 py-3 px-5">
+        <div className="mr-4  flex h-[66px] w-[66px] flex-col items-center justify-center  rounded-2xl border-2 border-dashed border-gray-400 py-3 px-5">
           <img className=" mb-1" src={Camera} alt="camera" />
-          <p className=" text-xs text-gray-400">0/1</p>
+          <p className=" text-xs text-gray-400">
+            <span className={`text-${!currentImg ? 'gray-400' : 'primary-2'}`}>
+              {!currentImg ? '0' : '1'}
+            </span>
+            /1
+          </p>
         </div>
       </label>
       <input
+        disabled={currentImg !== null && true}
+        onChange={handleFile}
         className="hidden"
         id="file"
         type={'file'}
         accept="image/*;capture=camera"
       />
+      {currentImg && (
+        <div className=" relative h-[66px] w-[66px]">
+          <img
+            className=" h-full w-full rounded-2xl"
+            src={currentImg}
+            alt={currentImg}
+          />
+          <Xbutton
+            onClick={() => setCurrentImg(null)}
+            className=" absolute top-[6px] right-[6px] cursor-pointer"
+          />
+        </div>
+      )}
     </div>
   )
 }
