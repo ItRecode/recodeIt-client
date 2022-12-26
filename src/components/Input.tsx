a59d184f2243c9b4d824956c8c9dd322ba440935
waterglasses <hsk10271@gmail.com>
2022-12-26 22:56:18 +0900

    [FE-59] feat: Input 컴포넌트 확장 구현

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { ReactComponent as CloseIcon } from '@assets/icon_closed.svg'
 
 interface InputPropsType extends React.InputHTMLAttributes<HTMLInputElement> {
-  property?: 'default'
+  property?: 'default' | 'success' | 'error'
   name: string
   label?: string
   placeholder?: string
   value?: number | string
   message?: string
+  focus?: boolean
   autoFocus?: boolean
+  maxLength?: number
 }
 
 export default function Input({
@@ -18,20 +21,43 @@ export default function Input({
   value,
   message,
   autoFocus = true,
+  maxLength,
   ...props
 }: InputPropsType) {
+  const setClassNameByProperty = (property: string) => {
+    if (property === 'error') return 'border-b-sub-1'
+    if (property === 'success') return 'border-b-primary-1'
+
+    return 'border-b-grey-4'
+  }
+
+  const setMessageColorByProperty = (property: string) => {
+    return property === 'success' ? 'text-primary-2' : 'text-sub-1'
+  }
+
   return (
     <div>
-      <p className="pb-6">{label}</p>
-      <input
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        className={`w-full border-b border-b-grey-4 pt-4 pb-4 placeholder:text-sm`}
-        autoFocus={autoFocus}
-        {...props}
-      />
-      <p className="pt-3 text-sm text-primary-2">{message}</p>
+      {label && <p className="pb-6">{label}</p>}
+      <div className="relative flex items-center">
+        <input
+          type="search"
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          autoFocus={autoFocus}
+          maxLength={maxLength}
+          className={`w-full border-b border-solid pt-4 pb-4 placeholder:text-sm 
+          focus:outline-none ${setClassNameByProperty(property)}
+        `}
+          {...props}
+        />
+        <CloseIcon className="absolute right-0 hover:cursor-pointer" />
+      </div>
+      {property !== 'default' && (
+        <p className={`pt-3 text-sm ${setMessageColorByProperty(property)}`}>
+          {message}
+        </p>
+      )}
     </div>
   )
 }
