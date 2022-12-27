6a6cc8d0ff4d34e9e11663049adfeaa342b2b031
Seongtaek Hwang <hst1993@hanmail.net>
2022-12-27 20:43:47 +0900

    [FE-40] feat: 레코드 상세 페이지 - 헤더

---
+++ src/components/MoreButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { ReactComponent as More } from '@assets/more.svg'
+
+export default function MoreButton() {
+  return <More>MoreButton</More>
+}
--- src/pages/DetailRecord/DetailRecord.tsx
+++ src/pages/DetailRecord/DetailRecord.tsx
@@ -1,5 +1,15 @@
+import BackButton from '@components/BackButton'
+import MoreButton from '@components/MoreButton'
 import React from 'react'
 
 export default function DetailRecord() {
-  return <div>DetailRecord</div>
+  return (
+    <div>
+      <header className="h-[60px]" />
+      <nav className="flex justify-between px-6">
+        <BackButton />
+        <MoreButton />
+      </nav>
+    </div>
+  )
 }
