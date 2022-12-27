f72c7707bbb647695f04c34f3dffe678650ccfff
moseung <tmdah900@naver.com>
2022-12-27 16:37:26 +0900

    [FE-46]refactor: 코드 리팩토링 -기존에 AddRecordIcon페이지에서 상수를 선언하고 사용하던걸 상수 파일로 분리

--- src/pages/AddRecord/AddRecordIcon.tsx
+++ src/pages/AddRecord/AddRecordIcon.tsx
@@ -1,17 +1,5 @@
+import { ADD_RECORD_ICONS } from '@assets/constant/RecordIcons'
 import React, { useState } from 'react'
-import Test from '@assets/record_icons/heart.svg'
-import Gift from '@assets/record_icons/gift.svg'
-import Music from '@assets/record_icons/music.svg'
-import Rocket from '@assets/record_icons/rocket.svg'
-import Like from '@assets/record_icons/like.svg'
-import Crown from '@assets/record_icons/crown.svg'
-import Medal from '@assets/record_icons/medal.svg'
-import Moon from '@assets/record_icons/moon.svg'
-import Speechbubble from '@assets/record_icons/speechbubble.svg'
-import Wine from '@assets/record_icons/wine.svg'
-import Umbrella from '@assets/record_icons/umbrella.svg'
-import Trashcan from '@assets/record_icons/trashcan.svg'
-import Lock from '@assets/record_icons/lock.svg'
 
 type IconSource = {
   src: string
@@ -29,25 +17,7 @@ function AddRecordIcon({
 }: {
   currentRecordType: keyof IconType
 }) {
-  const icons = {
-    celebration: [
-      { src: Test, choosed: true, id: 0 },
-      { src: Gift, choosed: false, id: 1 },
-      { src: Music, choosed: false, id: 2 },
-      { src: Rocket, choosed: false, id: 3 },
-      { src: Like, choosed: false, id: 4 },
-      { src: Crown, choosed: false, id: 5 },
-      { src: Medal, choosed: false, id: 6 },
-    ],
-    consolation: [
-      { src: Moon, choosed: true, id: 0 },
-      { src: Speechbubble, choosed: false, id: 1 },
-      { src: Wine, choosed: false, id: 2 },
-      { src: Umbrella, choosed: false, id: 3 },
-      { src: Trashcan, choosed: false, id: 4 },
-      { src: Lock, choosed: false, id: 5 },
-    ],
-  }
+  const icons = ADD_RECORD_ICONS
 
   const [iconState, setIconState] = useState<IconType>(icons)
 
