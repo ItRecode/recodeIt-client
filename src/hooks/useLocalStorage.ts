6cbb4d9181cad6e20190666d89b41f5aa71643a5
waterglasses <hsk10271@gmail.com>
2022-12-28 01:23:24 +0900

    [FE-49] refactor: 로컬 스토리지 커스텀 훅으로 수정

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,31 @@
+const useLocalStorage = () => {
+  const getItem = <T>(key: string, defaultValue: T): T => {
+    try {
+      const value = localStorage.getItem(key)
+      return value === null ? defaultValue : (JSON.parse(value) as T)
+    } catch (error) {
+      console.error(error)
+      return defaultValue
+    }
+  }
+
+  const setItem = <T>(key: string, value: T) => {
+    try {
+      localStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const removeItem = (key: string) => {
+    try {
+      localStorage.removeItem(key)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { getItem, setItem, removeItem }
+}
+
+export default useLocalStorage
