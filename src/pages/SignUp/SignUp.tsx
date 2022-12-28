aa3eba460a6eab2b9ffbb05fabef606433b91241
waterglasses <hsk10271@gmail.com>
2022-12-29 02:17:55 +0900

    [FE-33] feat: 닉네임 중복 useForm 적용

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -3,23 +3,46 @@ import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import Button from '@components/Button'
 import Input from '@components/Input'
+import useForm from '@hooks/useForm'
 
-type SingUpUserInfo = {
-  tempId: null | string
-  nickname: string
-}
+type PropertyType = 'default' | 'success' | 'error'
 
 export default function SignUp() {
   const location = useLocation()
-  const [userInfo, setUserInfo] = useState<SingUpUserInfo>({
-    tempId: null,
-    nickname: '',
+  const [tempId, setTempId] = useState<null | string>(null)
+  const [property, setProperty] = useState<PropertyType>('default')
+  const { errors, handleChange, handleSubmit } = useForm({
+    initialValues: {
+      nickname: '',
+    },
+    onSubmit: ({ nickname }) => {
+      // console.log(nickname)
+      // 닉네임 중복 확인
+      // nickname.trim()
+    },
+    validate: ({ nickname }) => {
+      console.log(nickname)
+
+      const error: { nickname?: string } = {}
+      const spacePattern = /\s/g
+      const nicknamePattern = /^[a-zA-Z\\d`~!@#$%^&*()-_=+]{2,8}$/
+
+      if (!nickname.match(spacePattern)) {
+        error.nickname = '공백을 제거해주세요'
+      }
+
+      if (nicknamePattern.test(nickname)) {
+        error.nickname = '국문, 영문, 숫자 포함 2~8자로 입력해주세요'
+      }
+
+      return error
+    },
   })
 
   useEffect(() => {
     if (location.state.tempSessionId) {
-      const tempId = location.state.tempSessionId
-      setUserInfo({ ...userInfo, tempId })
+      const tempSessionId = location.state.tempSessionId
+      setTempId(tempSessionId)
     }
   }, [])
 
@@ -30,18 +53,23 @@ export default function SignUp() {
         <br />
         사용하시겠어요?
       </h1>
-      <Input
-        label="닉네임"
-        name="nickname"
-        placeholder="국문, 영문, 숫자 포함 3~8자"
-        message="사용가능한 닉네임이에요"
-      />
-      <div className="mt-[72px] flex flex-col items-center gap-2">
-        <Button active={false}>중복 확인</Button>
-        <Button property="solid" active={false}>
-          레코딧 입장
-        </Button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          property={property}
+          label="닉네임"
+          name="nickname"
+          placeholder="국문, 영문, 숫자 포함 2~8자"
+          message={errors.nickname as string}
+          maxLength={8}
+          onChange={handleChange}
+        />
+        <div className="mt-[72px] flex flex-col items-center gap-2">
+          <Button active={true}>중복 확인</Button>
+          <Button property="solid" active={false}>
+            레코딧 입장
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
