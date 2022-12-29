e0025daedffea52b68ed684486dd99aaa598dd48
moseung <tmdah900@naver.com>
2022-12-29 14:35:51 +0900

    [FE-45]refactor: 불필요한 함수제거 -useCallback 사용안해서 제거

--- src/pages/AddRecord/AddRecordInput.tsx
+++ src/pages/AddRecord/AddRecordInput.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useCallback, useState } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import { INPUT_DETAILS } from '@assets/constant/constant'
 import { CheckAllType } from './AddRecord'
 
