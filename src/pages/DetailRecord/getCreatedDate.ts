03b9d23b046fcc9bac96903dec7db7792005afb6
Seongtaek Hwang <hst1993@hanmail.net>
2022-12-30 19:30:24 +0900

    [FE-69] feat: 날짜 전환 로직 구현 (초단위 추가)

--- src/pages/DetailRecord/getCreatedDate.ts
+++ src/pages/DetailRecord/getCreatedDate.ts
@@ -2,19 +2,27 @@ export const getCreatedDate = (created_at: string) => {
   const current_time = new Date().getTime()
   const created_time = new Date(new Date(created_at)).getTime()
   const difference_time = current_time - created_time / 1000
-  if (difference_time / 3600 < 1) {
-    const date = new Intl.RelativeTimeFormat('ko-KR').format(
-      Math.floor(-difference_time / 60),
+
+  if (difference_time < 60) {
+    const second = new Intl.RelativeTimeFormat('ko-KR').format(
+      Math.floor(-difference_time),
+      'second'
+    )
+    return second
+  }
+  if (difference_time * 60 < 60) {
+    const minute = new Intl.RelativeTimeFormat('ko-KR').format(
+      Math.floor(-difference_time * 60),
       'minute'
     )
-    return date
+    return minute
   }
   if (difference_time < 24) {
-    const date = new Intl.RelativeTimeFormat('ko-KR').format(
-      Math.floor(-difference_time / 3600),
+    const hour = new Intl.RelativeTimeFormat('ko-KR').format(
+      Math.floor(-difference_time * 3600),
       'hour'
     )
-    return date
+    return hour
   }
   if (difference_time > 24) {
     const date = new Intl.DateTimeFormat('ko-KR', {
