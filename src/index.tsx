4fe7fa2142fe7d3540e3c1353411df284e42b4d2
waterglasses <hsk10271@gmail.com>
2022-12-30 11:40:48 +0900

    [FE-63] style: StrictMode 삭제 복구

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,8 @@ import App from './App'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
