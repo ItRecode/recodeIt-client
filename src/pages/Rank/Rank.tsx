98fe7f8cffe59664e3fb074a1dce463b0b84988b
waterglasses <hsk10271@gmail.com>
2023-01-11 17:49:02 +0900

    [FE-105] feat: NavBar 탭에 Notservice 디자인 적용

--- src/pages/Rank/Rank.tsx
+++ src/pages/Rank/Rank.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { ReactComponent as NotServiceIcon } from '@assets/umbrella_big.svg'
 
 export default function Rank() {
-  const navigate = useNavigate()
-  useEffect(() => {
-    navigate('/notservice', { replace: true })
-  }, [])
-  return <div>Rank</div>
+  return (
+    <div className="flex h-full flex-col items-center justify-center px-5">
+      <NotServiceIcon className=" mb-10" />
+      <p className="mb-4 font-semibold text-grey-10">
+        아직 준비 중인 서비스예요
+      </p>
+      <p className="mb-10 text-xs font-medium">서비스가 완성되면 만나요.</p>
+    </div>
+  )
 }
