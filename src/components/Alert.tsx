5a101532ab516580e31207e808e029391e1c91c8
waterglasses <hsk10271@gmail.com>
2023-01-14 00:52:05 +0900

    [FE-101] fix: Alert 컴포넌트 Modal 적용

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,19 +1,23 @@
-import React, { ReactNode } from 'react'
-
+import React, { ReactNode, useState } from 'react'
+import Modal from './Modal'
 interface IAlertProps {
+  visible: boolean
   mainMessage: ReactNode
   subMessage?: string
   cancelMessage: string
   confirmMessage: string
+  onClose: () => void
   onCancel: () => void
   onConfirm: () => void
 }
 
 export default function Alert({
+  visible,
   mainMessage,
   subMessage,
   cancelMessage,
   confirmMessage,
+  onClose,
   onCancel,
   onConfirm,
 }: IAlertProps) {
@@ -21,24 +25,29 @@ export default function Alert({
     'h-full w-1/2 cursor-pointer bg-transparent py-4 text-base font-semibold'
 
   return (
-    <div className="flex h-[171px] w-[270px] flex-col justify-center">
-      <div className="flex flex-col items-center justify-center border-b border-grey-2 py-6 text-center">
-        {mainMessage}
-        {subMessage && (
-          <p className="pt-4 text-xs font-medium text-grey-8">{subMessage}</p>
-        )}
+    <Modal visible={visible} onClose={onClose}>
+      <div className="flex h-[171px] w-[270px] flex-col justify-center">
+        <div className="flex flex-col items-center justify-center border-b border-grey-2 py-6 text-center">
+          {mainMessage}
+          {subMessage && (
+            <p className="pt-4 text-xs font-medium text-grey-8">{subMessage}</p>
+          )}
+        </div>
+        <div className="flex justify-center align-middle">
+          <button
+            className={`${buttonClassName} text-grey-8`}
+            onClick={onCancel}
+          >
+            {cancelMessage}
+          </button>
+          <button
+            className={`${buttonClassName} text-primary-2`}
+            onClick={onConfirm}
+          >
+            {confirmMessage}
+          </button>
+        </div>
       </div>
-      <div className="flex justify-center align-middle">
-        <button className={`${buttonClassName} text-grey-8`} onClick={onCancel}>
-          {cancelMessage}
-        </button>
-        <button
-          className={`${buttonClassName} text-primary-2`}
-          onClick={onConfirm}
-        >
-          {confirmMessage}
-        </button>
-      </div>
-    </div>
+    </Modal>
   )
 }
