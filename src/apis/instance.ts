5413d83dd88fedc48c459d9be59a19356d0755d3
waterglasses <hsk10271@gmail.com>
2023-01-15 16:08:06 +0900

    [FE-109] refactor: 사용하지 않는 interceptor과 authInstance 제거

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,31 +1,10 @@
-import { UNAUTHORIZED_CODE } from '@assets/constant/constant'
-import axios, { AxiosInstance } from 'axios'
-import { redirect } from 'react-router-dom'
+import axios from 'axios'
 
 const { REACT_APP_DEV_API_END_POINT } = process.env
-
-function setInterceptors(instance: AxiosInstance) {
-  instance.interceptors.request.use((config) => {
-    return config
-  })
-
-  instance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-      if (error.response?.status === UNAUTHORIZED_CODE) {
-        redirect('/login')
-      }
-
-      return Promise.reject(error)
-    }
-  )
-  return instance
-}
 
 const baseInstance = axios.create({
   baseURL: REACT_APP_DEV_API_END_POINT,
   withCredentials: true,
 })
-const authInstance = setInterceptors(baseInstance)
 
-export { baseInstance, authInstance }
+export { baseInstance }
