f92171f2c6b7d3f1634f9461822ee6ae68a251f6
waterglasses <hsk10271@gmail.com>
2023-01-15 23:41:37 +0900

    [FE-120] feat: Axios instance에 timeout 적용

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -5,6 +5,7 @@ const { REACT_APP_DEV_API_END_POINT } = process.env
 const baseInstance = axios.create({
   baseURL: REACT_APP_DEV_API_END_POINT,
   withCredentials: true,
+  timeout: 5000,
 })
 
 export { baseInstance }
