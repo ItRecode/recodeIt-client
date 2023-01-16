79569929707df27821db6d3492c1ba1a91e70d6f
moseung <tmdah900@naver.com>
2023-01-16 13:40:51 +0900

    [FE-93]feat: 레코드 추가 사진 여러장 구현 -사진 한장> 여러장으로 구현가능하도록 다시 만들었습니다

--- src/pages/AddRecord/AddRecordFile.tsx
+++ src/pages/AddRecord/AddRecordFile.tsx
@@ -5,21 +5,22 @@ import { ReactComponent as DeleteIcon } from '@assets/deleteIcon.svg'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 interface Props {
   currentRecordType: string
-  setFiles: Dispatch<SetStateAction<File | undefined>>
+  setFiles: Dispatch<SetStateAction<File[]>>
+  files: File[]
 }
 
-function AddRecordFile({ currentRecordType, setFiles }: Props) {
-  const [currentImg, setCurrentImg] = useState<null | string>(null)
+function AddRecordFile({ currentRecordType, setFiles, files }: Props) {
+  const [currentImg, setCurrentImg] = useState<string[]>([])
+  const MAX_FILE = 3
 
   const handleSelectImageFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     encodeFileToBase64((e.target.files as FileList)[0])
-    setFiles((e.target.files as FileList)[0])
-    setCurrentImg(e.target.value)
+    setFiles([...files, (e.target.files as FileList)[0]])
     e.target.value = ''
   }
 
   useEffect(() => {
-    setCurrentImg(null)
+    setCurrentImg([])
   }, [currentRecordType])
 
   const encodeFileToBase64 = (fileBlob: File) => {
@@ -28,7 +29,7 @@ function AddRecordFile({ currentRecordType, setFiles }: Props) {
     return new Promise<void>((resolve, reject) => {
       reader.onload = () => {
         try {
-          setCurrentImg(reader.result as string | null)
+          setCurrentImg([...currentImg, reader.result as string])
           resolve()
         } catch (error) {
           reject(error)
@@ -37,47 +38,58 @@ function AddRecordFile({ currentRecordType, setFiles }: Props) {
     })
   }
 
-  const handleDelete = (): void => {
-    setCurrentImg(null)
-    setFiles(undefined)
+  const handleDelete = (toDeleteIndex: number): void => {
+    setCurrentImg(filterArray(currentImg, toDeleteIndex))
+    setFiles(filterArray(files, toDeleteIndex))
+  }
+
+  function filterArray<T>(array: Array<T>, toDeleteIndex: number): T[] {
+    return array.filter((value, index) => {
+      if (index !== toDeleteIndex) {
+        return value
+      }
+    })
   }
 
   return (
-    <div className="mb-8 flex items-center">
-      <label htmlFor="file">
+    <div className="mb-8 flex items-center gap-2">
+      <label className="h-[66px] w-[66px]" htmlFor="file">
         <div className="mr-4  flex h-[66px] w-[66px] flex-col items-center justify-center  rounded-2xl border-2 border-dashed border-grey-4 py-3 px-5">
           <img className=" mb-1" src={Camera} alt="camera" />
           <p className=" text-xs text-grey-4">
             <span
               className={`${!currentImg ? 'text-grey-4' : 'text-primary-2'}`}
             >
-              {!currentImg ? '0' : '1'}
+              {currentImg.length}
             </span>
-            /1
+            {`/${MAX_FILE}`}
           </p>
         </div>
       </label>
       <input
-        disabled={currentImg !== null}
+        disabled={currentImg.length === 3}
         onChange={handleSelectImageFile}
         className="hidden"
         id="file"
         type={'file'}
         accept=".jpg, .jpeg, .png, .svg, image/*;capture=camera"
       />
-      {currentImg && (
-        <div className="relative h-[66px] w-[66px]">
-          <img
-            className="h-full w-full rounded-2xl object-cover"
-            src={currentImg}
-            alt="user-selected-record-image"
-          />
-          <DeleteIcon
-            onClick={handleDelete}
-            className=" absolute top-[6px] right-[6px] cursor-pointer"
-          />
-        </div>
-      )}
+      {currentImg.length > 0 &&
+        currentImg.map((imgSrc, index) => {
+          return (
+            <div key={index} className="relative h-[66px] w-[66px]">
+              <img
+                className="h-full w-full rounded-2xl object-cover"
+                src={imgSrc}
+                alt="user-selected-record-image"
+              />
+              <DeleteIcon
+                onClick={() => handleDelete(index)}
+                className=" absolute top-[6px] right-[6px] cursor-pointer"
+              />
+            </div>
+          )
+        })}
     </div>
   )
 }
