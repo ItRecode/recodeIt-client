dbf6609f2bd58d10a98d870812fdc75686397f14
waterglasses <hsk10271@gmail.com>
2023-01-18 21:39:24 +0900

    [FE-141] feat: 마이 레코드 검색 인풋 레이아웃 구현

--- src/pages/MyRecord/MyRecord.tsx
+++ src/pages/MyRecord/MyRecord.tsx
@@ -10,17 +10,20 @@ export default function MyRecord() {
 
   return (
     <div className="h-full w-full px-6 pt-4">
-      <div className="relative flex items-center">
-        <SearchIcon className="absolute left-[12px] h-[14px] w-[14px]" />
-        <input
-          className="w-full rounded-[10px] bg-grey-2 py-[10px] pl-[38px] text-sm font-medium leading-[14px] placeholder:text-grey-5"
-          id="search-record-input"
-          type="search"
-          placeholder="레코드 제목을 입력하세요"
-          onClick={() => navigate('/notservice')}
-        />
-        <CloseIcon className="absolute right-[10px] cursor-pointer" />
-      </div>
+      <section className="sticky top-0 left-0" id="search-bar">
+        <div className="relative flex items-center">
+          <SearchIcon className="absolute left-[12px] h-[14px] w-[14px]" />
+          <input
+            className="w-full rounded-[10px] bg-grey-2 py-[10px] pl-[38px] text-sm font-medium leading-[14px] placeholder:text-grey-5"
+            id="search-record-input"
+            type="search"
+            placeholder="레코드 제목을 입력하세요"
+            onClick={() => navigate('/notservice')}
+          />
+          <CloseIcon className="absolute right-[10px] cursor-pointer" />
+        </div>
+      </section>
+
       <h1 className="mt-7 text-2xl font-semibold">마이 레코드</h1>
       <TodayRecord />
       <MemoryRecord />
