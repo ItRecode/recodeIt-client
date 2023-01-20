654bce1ff88e633190944e528962e94a97f7ff01
waterglasses <hsk10271@gmail.com>
2023-01-21 02:03:41 +0900

    [FE-142] feat: 나만의 추억 레코드 무한 스크롤로 구현

--- src/pages/MyRecord/MemoryRecord.tsx
+++ src/pages/MyRecord/MemoryRecord.tsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { IMemoryRecord } from 'types/recordData'
 import MemoryRecordItem from './MemoryRecordItem'
 import { useMemoryRecord } from '@react-query/hooks/useMemoryRecord'
 import { ReactComponent as Gift } from '@assets/record_icons/gift.svg'
 import { ReactComponent as ArrowDown } from '@assets/myRecordIcon/arrow_down.svg'
+import Spinner from '@components/Spinner'
 
 export default function MemoryRecord() {
-  const { memoryRecord, isLoading, prefetchMemoryRecord } = useMemoryRecord()
-  const [page, setPage] = useState(0)
-
-  useEffect(() => {
-    if (!isLoading && memoryRecord) {
-      if (memoryRecord.hasNextPage) {
-        prefetchMemoryRecord(page + 1)
-        setPage(page + 1)
-      }
-    }
-  }, [memoryRecord?.hasNextPage])
+  const {
+    memoryRecord,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useMemoryRecord()
 
   if (isLoading) {
     return <></>
@@ -40,24 +37,39 @@ export default function MemoryRecord() {
 
   return (
     <>
-      {memoryRecord.memoryRecordList?.map((memoryRecord: IMemoryRecord) => (
-        <MemoryRecordItem
-          key={memoryRecord.recordId}
-          recordId={memoryRecord.recordId}
-          title={memoryRecord.title}
-          iconName={memoryRecord.iconName}
-          iconColor={memoryRecord.iconColor}
-          commentList={memoryRecord.commentList}
-        />
-      ))}
-      {memoryRecord.hasNextPage && !memoryRecord.isLastPage && (
-        <button className="relative mb-[130px] w-full cursor-pointer border-t border-t-grey-3 bg-grey-1 py-4">
-          <div className="flex items-center justify-center">
-            <span className="text-sm text-primary-2">더보기</span>
-            <ArrowDown className="ml-[10px]" />
-          </div>
-        </button>
+      {memoryRecord.pages.map(({ data }) =>
+        data.memoryRecordList.map((memoryRecord: IMemoryRecord) => (
+          <MemoryRecordItem
+            key={memoryRecord.recordId}
+            recordId={memoryRecord.recordId}
+            title={memoryRecord.title}
+            iconName={memoryRecord.iconName}
+            iconColor={memoryRecord.iconColor}
+            commentList={memoryRecord.commentList}
+          />
+        ))
       )}
+      <div className="relative mb-[130px] h-[160px]">
+        {hasNextPage && (
+          <button
+            className={`w-full bg-grey-1 ${
+              !isFetchingNextPage && 'cursor-pointer border-t border-t-grey-3'
+            }`}
+            onClick={() => fetchNextPage()}
+          >
+            <div className="flex items-center justify-center py-2">
+              {isFetchingNextPage ? (
+                <Spinner size="button" />
+              ) : (
+                <div className="py-2">
+                  <span className="text-sm text-primary-2">더보기</span>
+                  <ArrowDown className="ml-[10px]" />
+                </div>
+              )}
+            </div>
+          </button>
+        )}
+      </div>
     </>
   )
 }
--- src/react-query/hooks/useMemoryRecord.ts
+++ src/react-query/hooks/useMemoryRecord.ts
@@ -1,36 +1,35 @@
 import { QUERY_KEYS } from '@react-query/queryKeys'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { getMemoryRecord } from '@apis/record'
-import { useState } from 'react'
 
 export const useMemoryRecord = () => {
-  const queryClient = useQueryClient()
-  const [pageParam, setPageParam] = useState(0)
+  const {
+    data: memoryRecord = null,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: [QUERY_KEYS.memoryRecord],
+    queryFn: async ({ pageParam = 0 }) => await getMemoryRecord(pageParam),
+    getNextPageParam: (lastPage): number | null => {
+      const { data, config } = lastPage
+      if (data.hasNextPage && !data.isLastPage) {
+        return config.params.pageNum + 1
+      }
+      return null
+    },
+    retry: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  })
 
-  const queryFnByMemoryRecord = async () => {
-    const { data } = await getMemoryRecord(pageParam)
-    return data
+  return {
+    memoryRecord,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
   }
-
-  const { data: memoryRecord = null, isLoading } = useQuery(
-    [QUERY_KEYS.memoryRecord, pageParam],
-    queryFnByMemoryRecord,
-    {
-      retry: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-    }
-  )
-
-  const prefetchMemoryRecord = (nextPageParams: number) => {
-    queryClient.prefetchQuery(
-      [QUERY_KEYS.memoryRecord, nextPageParams],
-      queryFnByMemoryRecord
-    )
-  }
-
-  console.log(memoryRecord)
-
-  return { memoryRecord, isLoading, prefetchMemoryRecord, setPageParam }
 }
