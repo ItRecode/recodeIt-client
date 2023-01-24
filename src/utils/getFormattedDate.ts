e86ad9c778a1e9e6245b64180992ab13cb517770
waterglasses <hsk10271@gmail.com>
2023-01-24 14:41:19 +0900

    [FE-150] remove: 사용하지 않는 변수 제거

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -1,12 +1,11 @@
 const DATE_JOIN_POINT = 'point'
-const DATE_JOIN_HYPHEN = 'Hyphen'
 
 export const getFormattedDate = (date: Date, type: string) => {
   const year = date.getFullYear()
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
 
-  if (type === DATE_JOIN_HYPHEN) {
+  if (type === DATE_JOIN_POINT) {
     return `${year}.${month}.${day}`
   }
 
