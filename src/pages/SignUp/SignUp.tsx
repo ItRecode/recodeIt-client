a051254811201e50ab6107d9b4ab7e28e53facf9
waterglasses <hsk10271@gmail.com>
2023-01-29 13:49:00 +0900

    [FE-165] feat: 회원가입 이모지 안들어가도록 구현

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -46,6 +46,8 @@ export default function SignUp() {
     const spacePattern = /\s/g
     const consonantAndVowelPattern = /[ㄱ-ㅎㅏ-ㅣ]/
     const specialPattern = /[`~!@#$%^&*()_|+\-=?;:'",.<>\\{}[\]\\/₩]/gim
+    const emojiPattern =
+      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g
 
     if (nickname.length < NICKNAME_MIN_LENGTH) {
       setErrorMessage(`${NICKNAME_MIN_LENGTH}글자 이상 입력해주세요.`)
@@ -64,6 +66,11 @@ export default function SignUp() {
 
     if (nickname.match(consonantAndVowelPattern)) {
       setErrorMessage('자음이나 모음만은 사용할 수 없어요.')
+      return false
+    }
+
+    if (nickname.match(emojiPattern)) {
+      setErrorMessage('이모지는 사용할 수 없어요.')
       return false
     }
 
