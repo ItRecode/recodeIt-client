d98cc1a5c13c4899ed8742655c61688ddd5fc0f5
waterglasses <hsk10271@gmail.com>
2023-02-10 12:35:04 +0900

    [FE-202] design: 마일 레코드 페이지 변경된 레이아웃으로 재구현

--- src/pages/MyRecord/MyRecord.tsx
+++ src/pages/MyRecord/MyRecord.tsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ReactComponent as CloseIcon } from '@assets/icon_closed.svg'
 import { ReactComponent as SearchIcon } from '@assets/myRecordIcon/search.svg'
+import { ReactComponent as CalendarIcon } from '@assets/myRecordIcon/calendar.svg'
+import { useMyRecord } from '@react-query/hooks/useMyRecord'
 import TodayRecord from './TodayRecord'
 import MemoryRecord from './MemoryRecord'
-import MyRecordCalendar from './MyRecordCalendar'
-import { useMyRecord } from '@react-query/hooks/useMyRecord'
 import Calendar from './Calendar'
 
 export default function MyRecord() {
@@ -36,12 +36,24 @@ export default function MyRecord() {
             <CloseIcon className="absolute right-[10px] cursor-pointer" />
           </div>
         </section>
-        <MyRecordCalendar setIsOpenCalendar={setIsOpenCalendar} />
+        <section id="my-today-record">
+          <div className="mt-3 px-6">
+            <h1 className="text-2xl font-semibold">마이 레코드</h1>
+          </div>
+        </section>
         <TodayRecord />
-        <h2 className="mt-3 px-6 text-lg font-semibold leading-[18px]">
-          나만의 추억 레코드
-        </h2>
-        <MemoryRecord />
+        <section id="my-memory-record">
+          <div className="mt-3 flex items-center justify-between px-6">
+            <h2 className="text-lg font-semibold leading-[18px]">
+              나만의 추억 레코드
+            </h2>
+            <CalendarIcon
+              className="cursor-pointer"
+              onClick={() => setIsOpenCalendar(true)}
+            />
+          </div>
+          <MemoryRecord />
+        </section>
       </div>
       {isOpenCalendar && (
         <Calendar monthYear={monthYear} setIsOpenCalendar={setIsOpenCalendar} />
