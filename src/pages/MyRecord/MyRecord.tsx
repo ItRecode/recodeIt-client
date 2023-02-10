06fb5387e0aa2c996ce4a65c6bf041d40c85a6a9
waterglasses <hsk10271@gmail.com>
2023-02-10 12:53:58 +0900

    [FE-202] feat: onKeyUp이벤트로 엔터 시 검색 페이지로 이동

--- src/pages/MyRecord/MyRecord.tsx
+++ src/pages/MyRecord/MyRecord.tsx
@@ -9,9 +9,16 @@ import MemoryRecord from './MemoryRecord'
 import Calendar from './Calendar'
 
 export default function MyRecord() {
-  const [isOpenCalendar, setIsOpenCalendar] = useState(false)
   const navigate = useNavigate()
   const { isLoading, monthYear } = useMyRecord()
+  const [isOpenCalendar, setIsOpenCalendar] = useState(false)
+  const [keyword, setKeyword] = useState('')
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      navigate('/notservice')
+    }
+  }
 
   if (isLoading) {
     return <></>
@@ -27,11 +34,11 @@ export default function MyRecord() {
           <div className="relative flex items-center">
             <SearchIcon className="absolute left-[12px] h-[14px] w-[14px]" />
             <input
-              className="w-full rounded-[10px] bg-grey-2 py-[10px] pl-[38px] text-sm font-medium leading-[14px] placeholder:text-grey-5"
+              className="w-full rounded-[10px] bg-grey-2 py-[10px] pl-[38px] text-sm font-medium leading-[14px] outline-none placeholder:text-grey-5"
               id="search-record-input"
-              type="search"
               placeholder="레코드 제목을 입력하세요"
-              onClick={() => navigate('/notservice')}
+              onChange={(e) => setKeyword(e.target.value)}
+              onKeyUp={handleSearch}
             />
             <CloseIcon className="absolute right-[10px] cursor-pointer" />
           </div>
