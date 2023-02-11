fdda6c1e8acb7c20a9700afbe54999e6eccad116
waterglasses <hsk10271@gmail.com>
2023-02-12 00:40:21 +0900

    [FE-201] feat: 마이 레코드 검색 API 적용

--- src/pages/MyRecord/Search/SearchRecord.tsx
+++ src/pages/MyRecord/Search/SearchRecord.tsx
@@ -3,19 +3,23 @@ import { useLocation } from 'react-router-dom'
 import BackButton from '@components/BackButton'
 import SearchInput from '../Common/SearchInput'
 import MyRecordCard from '../Common/MyRecordCard'
+import useDebounce from '@hooks/useDebounce'
+import { useMyRecordByKeyword } from '@react-query/hooks/useMyRecordByKeyword'
 
 export default function SearchRecord() {
-  const { state } = useLocation()
+  const { state } = useLocation() // 전역에 저장해놓기 => 검색어 없으면 메인으로 돌리기..?
   const [keyword, setKeyword] = useState(state || '')
-  const record = {
-    categoryName: '축하해주세요',
-    recordId: 276,
-    title: 'ㅎㅇㅎ',
-    iconName: 'gift',
-    colorName: 'icon-yellow',
-    createdAt: '2023-02-11T14:22:21.853545',
-    commentCount: 0,
-  }
+  const { myRecordByKeyword, setKeywordWithQuery } = useMyRecordByKeyword(state)
+
+  useDebounce(
+    () => {
+      if (keyword.length > 0) {
+        setKeywordWithQuery(keyword)
+      }
+    },
+    300,
+    [keyword]
+  )
 
   return (
     <>
@@ -35,15 +39,21 @@ export default function SearchRecord() {
         </div>
       </section>
       <section id="search-result-records" className="mt-4 mb-10 w-full px-6">
-        <MyRecordCard
-          recordId={record.recordId}
-          title={record.title}
-          categoryName={record.categoryName}
-          commentCount={record.commentCount}
-          iconName={record.iconName}
-          colorName={record.colorName}
-          createdAt={record.createdAt}
-        />
+        {myRecordByKeyword?.pages.map((page) =>
+          page.data.recordBySearchDtos.map((record) => (
+            <div className="mt-6" key={record.recordId}>
+              <MyRecordCard
+                recordId={record.recordId}
+                title={record.title}
+                categoryName={record.categoryName}
+                commentCount={record.commentCount}
+                iconName={record.iconName}
+                colorName={record.colorName}
+                createdAt={record.createdAt}
+              />
+            </div>
+          ))
+        )}
       </section>
     </>
   )
