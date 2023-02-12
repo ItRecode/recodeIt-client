d388b7e17be335ccaceb3bf45054eaee334b356f
moseung <tmdah900@naver.com>
2023-02-12 09:52:47 +0900

    [FE-196]feat: 모아보기 레이아웃 구성 -전체적인 레이아웃 -최신페이지 레이아웃

--- src/pages/Collect/Collect.tsx
+++ src/pages/Collect/Collect.tsx
@@ -4,9 +4,8 @@ import RecentRecord from './RecentRecord'
 export default function Collect() {
   return (
     <div className="relative flex flex-col ">
-      <div className="h-[2000px] ">1</div>
+      <div className="h-[1000px] bg-primary-6">1</div>
       <RecentRecord />
-      <div className="h-[2000px] ">2</div>
     </div>
   )
 }
