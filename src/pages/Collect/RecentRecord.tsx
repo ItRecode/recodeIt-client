9b799bb66a63fc111cc9e189e9c8e620d4e3975d
moseung <tmdah900@naver.com>
2023-02-15 14:29:26 +0900

    [FE-220]fix: 새로고침시 화면 상단에 고정안되는 버그 수정

--- src/pages/Collect/RecentRecord.tsx
+++ src/pages/Collect/RecentRecord.tsx
@@ -6,7 +6,7 @@ import RecordCard from '@components/RecordCard'
 import { CategoryCard } from 'types/recordData'
 import { useRecentRecord } from '@react-query/hooks/useRecentRecord'
 import { useIntersect } from '@hooks/useIntersectionObserver'
-import { getCurrentTime } from '@utils/getCurrentTime'
+import { GetCurrentTime } from '@utils/getCurrentTime'
 import { LocalStorage } from '@utils/localStorage'
 
 function RecentRecord() {
@@ -14,10 +14,11 @@ function RecentRecord() {
     useRecentRecord()
   const [timer, setTimer] = useState(0)
   const interval: { current: NodeJS.Timeout | undefined } = useRef()
+  const recentRef: React.RefObject<HTMLDivElement> = useRef(null)
 
   useEffect(() => {
     const getTimer = LocalStorage.get('timer')
-    const RESET_TIME = 5
+    const RESET_TIME = 180
     const timeGapByTimer =
       getTimer !== null &&
       Math.floor((new Date().getTime() - JSON.parse(getTimer)) / 1000)
@@ -35,8 +36,6 @@ function RecentRecord() {
     }
   }, [timer])
 
-  const recentRef: React.RefObject<HTMLDivElement> = useRef(null)
-
   const scrollEndRef = useIntersect(async (entry, observer) => {
     observer.unobserve(entry.target)
     if (hasNextPage && !isLoading) {
@@ -48,77 +47,85 @@ function RecentRecord() {
     return <Spinner size="large" />
   }
 
-  const handleReset = () => {
-    reset()
-    LocalStorage.set('timer', JSON.stringify(new Date().getTime()))
-    setTimer(180)
+  const scrollRecentViewTop = () => {
     if (recentRef.current !== null) {
       recentRef.current.scrollIntoView({
-        behavior: 'smooth',
         block: 'start',
+        behavior: 'smooth',
       })
     }
   }
 
+  const handleReset = () => {
+    reset()
+    LocalStorage.set('timer', JSON.stringify(new Date().getTime()))
+    setTimer(180)
+    scrollRecentViewTop()
+  }
+
+  const getTime = () => {
+    const getCurrentTime = new GetCurrentTime()
+    return `${getCurrentTime.getHours()}: ${getCurrentTime.getMinutes()}`
+  }
+
   return (
-    <div className="relative px-6 pb-20">
-      <div className="sticky top-0  left-0 mb-5 bg-grey-1 py-6">
-        <div className="mb-5 flex items-center justify-between">
-          <div className="flex items-center   ">
-            <p className="mr-3 text-[18px] font-semibold">최신 레코드</p>
+    <div ref={recentRef}>
+      <div className="relative px-6 pb-20">
+        <div className="sticky top-0 left-0 mb-5 flex flex-col justify-center bg-grey-1 py-6">
+          <div className="mb-5 flex items-center justify-between">
+            <div className="flex items-center   ">
+              <p className="mr-3 text-[18px] font-semibold">최신 레코드</p>
+            </div>
+            <p className="text-xs font-medium text-grey-8">{`${getTime()} 기준`}</p>
           </div>
-          <p className="text-xs font-medium text-grey-8">
-            {`${getCurrentTime.getHours()}`}: {`${getCurrentTime.getMinutes()}`}{' '}
-            기준
-          </p>
+          <div className="flex items-center justify-between">
+            <p className="text-[14px] font-medium text-grey-8">
+              지금 축하받는 레코드는?
+            </p>
+            {timer === 0 ? (
+              <Reset onClick={handleReset} className="cursor-pointer" />
+            ) : (
+              <ResetDisabled />
+            )}
+          </div>
         </div>
-        <div className="flex items-center justify-between">
-          <p className="text-[14px] font-medium text-grey-8">
-            지금 축하받는 레코드는?
-          </p>
-          {timer === 0 ? (
-            <Reset onClick={handleReset} className="cursor-pointer" />
-          ) : (
-            <ResetDisabled />
-          )}
+        <div className="flex flex-wrap gap-2">
+          {recentRecord !== null &&
+            recentRecord.pages.map(({ data }) =>
+              data.content.map(
+                ({
+                  recordId,
+                  title,
+                  colorName,
+                  commentCount,
+                  iconName,
+                }: CategoryCard) => {
+                  const ColorName = `bg-${colorName}`
+                  return (
+                    <div
+                      className={`h-[200px] w-[calc(50%-4px)] rounded-2xl ${ColorName} flex justify-center `}
+                      key={recordId}
+                    >
+                      <RecordCard
+                        recordId={recordId}
+                        title={title}
+                        colorName={colorName}
+                        commentCount={commentCount}
+                        iconName={iconName}
+                      />
+                    </div>
+                  )
+                }
+              )
+            )}
         </div>
+        <div ref={scrollEndRef} className="h-10 w-full " />
+        {hasNextPage && (
+          <div className="flex w-full justify-center">
+            <Spinner size="small" />
+          </div>
+        )}
       </div>
-      <div ref={recentRef} className="flex flex-wrap gap-2">
-        {recentRecord !== null &&
-          recentRecord.pages.map(({ data }) =>
-            data.content.map(
-              ({
-                recordId,
-                title,
-                colorName,
-                commentCount,
-                iconName,
-              }: CategoryCard) => {
-                const ColorName = `bg-${colorName}`
-                return (
-                  <div
-                    className={`h-[200px] w-[calc(50%-4px)] rounded-2xl ${ColorName} flex justify-center `}
-                    key={recordId}
-                  >
-                    <RecordCard
-                      recordId={recordId}
-                      title={title}
-                      colorName={colorName}
-                      commentCount={commentCount}
-                      iconName={iconName}
-                    />
-                  </div>
-                )
-              }
-            )
-          )}
-      </div>
-      <div ref={scrollEndRef} className="h-10 w-full " />
-      {hasNextPage && (
-        <div className="flex w-full justify-center">
-          <Spinner size="small" />
-        </div>
-      )}
     </div>
   )
 }
