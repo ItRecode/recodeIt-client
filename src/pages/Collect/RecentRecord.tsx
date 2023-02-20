b3d180b6fcaeb4ddd6ebaffc3843eec336271ea3
moseung <tmdah900@naver.com>
2023-02-20 14:13:38 +0900

    [FE-228]fix: 시간 크기 변경되지 않도록 -숫자 하나의 크기를 고정값으로 둬서 구현

--- src/pages/Collect/RecentRecord.tsx
+++ src/pages/Collect/RecentRecord.tsx
@@ -62,7 +62,7 @@ function RecentRecord() {
 
   const getTime = () => {
     const getCurrentTime = new GetCurrentTime()
-    return `${getCurrentTime.getHours()}: ${getCurrentTime.getMinutes()}`
+    return `${getCurrentTime.getHours()} : ${getCurrentTime.getMinutes()}`
   }
 
   return (
@@ -79,7 +79,7 @@ function RecentRecord() {
             <p className="text-[14px] font-medium text-grey-8">
               지금 올라오고 있는 레코드는?
             </p>
-            <div className=" flex flex-col items-center justify-center">
+            <div className=" flex w-[10%] flex-col items-center justify-center">
               {timer === 0 ? (
                 <Reset onClick={handleReset} className="cursor-pointer" />
               ) : (
