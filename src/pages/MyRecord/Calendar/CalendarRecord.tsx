f41355417213d7ab6a54b1409a12c5d1396fe8d6
waterglasses <hsk10271@gmail.com>
2023-02-22 17:21:09 +0900

    [FE-208] feat: 마이레코드 - 달력 날짜 검색 시 상단 레이아웃 구현

--- src/pages/MyRecord/Calendar/CalendarRecord.tsx
+++ src/pages/MyRecord/Calendar/CalendarRecord.tsx
@@ -1,5 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Calendar from './Calendar'
+import BackButton from '@components/BackButton'
+import { ReactComponent as CalendarIcon } from '@assets/myRecordIcon/calendar.svg'
 
 export default function CalendarRecord() {
-  return <div>CalendarRecord</div>
+  const [isOpenCalendar, setIsOpenCalendar] = useState(false)
+
+  return (
+    <>
+      <section id="route-backIcon-button" className="ml-[18px] mt-4">
+        <BackButton />
+      </section>
+      <section id="selected-date-record">
+        <div className="mt-7 px-6">
+          <h1 className="text-2xl font-semibold">마이 레코드</h1>
+        </div>
+        <div className="mt-6 flex items-center justify-between px-6">
+          <h2 className="text-lg font-semibold leading-[18px]">
+            2023년 2월 1일의 추억 레코드
+          </h2>
+          <CalendarIcon
+            className="cursor-pointer"
+            onClick={() => setIsOpenCalendar(true)}
+          />
+        </div>
+      </section>
+      {isOpenCalendar && <Calendar setIsOpenCalendar={setIsOpenCalendar} />}
+    </>
+  )
 }
