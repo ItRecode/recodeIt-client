f4fea42a465ef0ae36fdb51f78ec00c5c9a99535
Seongtaek Hwang <hst1993@hanmail.net>
2023-02-25 10:05:08 +0900

    [FE-232] fix: 오타수정

--- src/pages/Setting/FeedbackMail/FeedbackMail.tsx
+++ src/pages/Setting/FeedbackMail/FeedbackMail.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function FeedbackMail() {
-  return <div>피백메</div>
+  return (
+    <div>
+      <a
+        href="mailto:hst92124@gmail.com&amp;subject=으아아아"
+        className="cursor-pointer"
+      >
+        피드백 보내기
+      </a>
+    </div>
+  )
 }
 
 export default FeedbackMail
