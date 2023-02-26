36b26b62c60e25e99d2a986d5026079d889c858b
waterglasses <hsk10271@gmail.com>
2023-02-26 14:28:51 +0900

    [FE-238] feat: 닉네임 변경 api 설정

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -3,3 +3,7 @@ import { baseInstance } from './instance'
 export const getUserInfo = () => {
   return baseInstance.get(`/member/auth`)
 }
+
+export const updateUserInfo = (nickname: string) => {
+  return baseInstance.put(`/member`, { params: { nickname } })
+}
