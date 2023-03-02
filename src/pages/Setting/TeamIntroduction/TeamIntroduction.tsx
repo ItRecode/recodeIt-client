aa8e9c9add5763d2bcbd543eae3d32aa4ba4949a
Seongtaek Hwang <hst1993@hanmail.net>
2023-03-02 14:58:09 +0900

    fix: 팀소개 페이지 레이아웃 수정

--- src/pages/Setting/TeamIntroduction/TeamIntroduction.tsx
+++ src/pages/Setting/TeamIntroduction/TeamIntroduction.tsx
@@ -11,7 +11,7 @@ function TeamIntroduction() {
       <div className="mt-6 aspect-[375/277] w-full bg-[url('@assets/teamIntroductionImage.svg')] bg-cover" />
       <div className="px-6 py-10">
         <p className="text-xl font-semibold leading-normal">
-          <span className="text-primary-2">텐저스</span>를 소개합니다!
+          <span className="text-primary-2">텐져스</span>를 소개합니다!
         </p>
         <div className="mt-4 text-sm text-grey-9">
           <p>감정 공유에 진심인 10명이 모여 감정을 기록할 수 있는</p>
@@ -29,7 +29,7 @@ function TeamIntroduction() {
           }
           className="mt-8 flex w-full cursor-pointer items-center justify-between bg-transparent p-0 text-base font-semibold"
         >
-          <p className="pt-1">
+          <p>
             더 자세한 팀 <span className="text-primary-2">텐져스의 이야기</span>{' '}
             보러가기
           </p>
