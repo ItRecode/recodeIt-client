c340fa80e98417ae3b5aa4cea4e3f86c245acc21
waterglasses <hsk10271@gmail.com>
2023-03-08 17:13:35 +0900

    [FE-252] feat: 탈퇴 전 닉네임 확인 페이지 레이아웃 구현

---
+++ src/pages/Setting/Withdraw/CheckedNicknameBeforeWithDraw.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { NICKNAME_MAX_LENGTH } from '@assets/constant/constant'
+import { ReactComponent as CloseIcon } from '@assets/icon_closed.svg'
+import { ReactComponent as Back } from '@assets/back.svg'
+
+export default function CheckedNicknameBeforeWithDraw() {
+  const navigate = useNavigate()
+  const { state } = useLocation()
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [isCheckedNickname, setIsCheckedNickname] = useState(false)
+  const [nickname, setNickname] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [inputBorderStyle, setInputBorderStyle] = useState('')
+  const [isFocusInput, setIsFocusInput] = useState(false)
+
+  return (
+    <>
+      <section id="route-backIcon-button" className="ml-[18px] mt-4">
+        <Back
+          className="cursor-pointer"
+          onClick={() => navigate('/setting', { replace: true })}
+        />
+      </section>
+      <section
+        id="checked-nickname-before-withdraw-info"
+        className="mt-11 px-6"
+      >
+        <h1 className="text-[20px] font-medium leading-[30px]">
+          계속 탈퇴를 진행하시려면
+          <br />
+          닉네임을 입력해 확인해주세요
+        </h1>
+        <div className="mt-10">
+          <p className="font-medium">닉네임</p>
+          <div className="relative mt-[9px] flex w-full items-center">
+            <input
+              ref={inputRef}
+              id="modify-nickname-input"
+              value={nickname}
+              placeholder={isFocusInput || !state ? '' : state.nickname}
+              className={`w-full rounded-[8px] bg-grey-2 py-[17px] pl-[12px] text-[14px] font-medium placeholder:text-grey-5 focus:outline-none
+              ${inputBorderStyle}`}
+              autoComplete="off"
+              onChange={(e) => setNickname(e.target.value)}
+              onFocus={() => setIsFocusInput(true)}
+              onBlur={() => setIsFocusInput(false)}
+              maxLength={NICKNAME_MAX_LENGTH}
+            />
+            <CloseIcon
+              className="absolute right-[10px] cursor-pointer"
+              onClick={() => {
+                setNickname('')
+                inputRef.current?.focus()
+              }}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
